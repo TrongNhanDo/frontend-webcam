0d0671629f4d47dffb38da65fb3b0dbc920e042e
dtnhan <dotrongnhan15102000@gmail.com>
2024-03-06 21:11:04 +0700

    fix: deploy to github page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { MainContextProvider } from "./context/MainContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <MainContextProvider>
       <App />
     </MainContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
