31634c8634cccdb083c10558696605994fbc1e18
dtnhan <dotrongnhan15102000@gmail.com>
2024-03-12 09:24:43 +0700

    fix: add condition to change source when run on the local and production environment

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import { MainContextProvider } from "./context/MainContext";
 
+const mode = process.env.REACT_APP_ENV || "local";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-    <MainContextProvider>
-      <App />
-    </MainContextProvider>
-  </HashRouter>
+  <>
+    {mode === "local" ? (
+      <HashRouter>
+        <MainContextProvider>
+          <App />
+        </MainContextProvider>
+      </HashRouter>
+    ) : (
+      <BrowserRouter>
+        <MainContextProvider>
+          <App />
+        </MainContextProvider>
+      </BrowserRouter>
+    )}
+  </>
 );
